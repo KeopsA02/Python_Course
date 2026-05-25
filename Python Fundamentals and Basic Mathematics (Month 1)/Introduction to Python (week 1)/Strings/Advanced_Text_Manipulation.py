aa63274e13a3f8f6ed6acf6b7de329d4b0451757
Keops <83623787+KeopsA02@users.noreply.github.com>
2026-05-25 10:44:29 -0600

    Delete Python Fundamentals and Basic Mathematics (Month 1) directory

--- Python Fundamentals and Basic Mathematics (Month 1)/Introduction to Python (week 1)/Strings/Advanced_Text_Manipulation.py
+++
@@ -1,5 +0,0 @@
-""" More powerful ways to work with text. """
-
-text = "python is great, python is powerful"
-print(f"Replace first only: {text.replace('python', 'Python', 1)}")
-# "Python is great, python is powerful"
